4d611cb2e9b3a48bc10bffe79794501d2545664f
Riley Sun <32978625+RileySun@users.noreply.github.com>
2022-02-08 16:40:09 +0100

    Fixed error with newlines.
    Adds more newlines now on header replacement.

---
+++ capitalize-post-title/capitalize-post-title.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+Plugin Name: Capitalize Post Title
+Plugin URI: https://github.com/rileysun/capitalize-post-title/
+Description: Capitalize post titles with the click of a button
+Version: 1.1
+Author: Riley Lesser
+Author URI: https://sun-sys.tk/
+*/
+
+class Capitalize {
+
+	public static function init() {
+		add_action('admin_enqueue_scripts', array(__CLASS__, 'capitalizeEnqueAdminScripts'));
+		add_action('post_submitbox_misc_actions', array(__CLASS__, 'capitalizeButton'));
+	}
+	
+	public static function capitalizeButton() {
+		wp_enqueue_script('capitalize-post-title');
+		echo '
+			<div style="display: block; width: 92%; margin: 1% auto 4%;">
+				<input type="button" id="Capitalize" class="button button-primary button-large" value="Capitalize" />
+			</div>
+			<div style="display: block; width: 92%; margin: 1% auto 4%;">
+				<input type="button" id="Replace-Headers" class="button button-primary button-large" value="Headers" />
+			</div>
+		';
+	}
+	
+	public static function capitalizeEnqueAdminScripts($hook) {
+		if ($hook == 'post.php' || $hook == 'post-new.php') {		
+			wp_register_script('capitalize-post-title', plugin_dir_url( __FILE__ ) . 'capitalize-post-title.js', array(), '1.0.0', 'all' );
+		}
+	}
+	
+}
+
+Capitalize::init();
